88da285908d29c782a8c7c60b1944a1a6ba31b7f
sharsulkar <salil.harsulkar@gmail.com>
2021-01-27 23:40:08 +0000

    Updated code for logging

--- src/myapp.py
+++ src/myapp.py
@@ -4,23 +4,40 @@ import build_features
 
 
 def main():
-    logging.basicConfig(filename='./myapp.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-    logging.info('Started')
+    # create logger with 'spam_application'
+    logger = logging.getLogger('my_application')
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('./myapp.log')
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    
+
+    logger.info('Execution started')
     #make_dataset
     input_df=make_dataset.main()
-    logging.info('Dataset imported')
+    logger.info('Dataset imported')
     #build_features
     X,y=build_features.main(input_df)
-    logging.info('Features built, X shape %d, y shape %d',X.shape,y.shape)
+    logger.info('Features built, X shape %d, y shape %d',X.shape,y.shape)
 
     #if mode==Train: 
         #train_model
-    logging.info('Model trained')
+    logger.info('Model trained')
     #elif mode==Predict:
         #make_prediction
-    logging.info('Prediction made')
+    logger.info('Prediction made')
     #explain_model_prediction    
-    logging.info('Model explained')
+    logger.info('Model explained')
 
 if __name__ == '__main__':
     main()
