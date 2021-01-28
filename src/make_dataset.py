48b99fff2a11f94b5c4210d5fcd13e279670bd75
sharsulkar <salil.harsulkar@gmail.com>
2021-01-28 00:05:56 +0000

    added code for logging

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import logging
 from mylib import read_csv_to_list
+import logging
 
+module_logger= logging.getLogger('my_application.make_dataset')
 def main():
-    #required_features=['a','b']
+
+    module_logger.info('Starting executiion of make_dataset module.')
+
     required_features=read_csv_to_list('https://raw.githubusercontent.com/sharsulkar/H1B_LCA_outcome_prediction/main/data/processed/required_features.csv',header=None,squeeze=True)
     try :
         input_df=pd.read_excel('https://www.dol.gov/sites/dolgov/files/ETA/oflc/pdfs/LCA_Disclosure_Data_FY2020_Q2.xlsx',usecols=required_features)
     except ValueError:
-        logging.exception('ValueError: columns %s not found',required_features)
+        module_logger.exception('ValueError: columns %s not found',required_features)
+    
+    module_logger.info('Executiion of make_dataset module complete.')
+
     return input_df
 
 if __name__ == '__main__':
