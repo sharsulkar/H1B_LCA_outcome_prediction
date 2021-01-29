0312ceafe8d29925e6947fc65afd4360a4d6a2c1
sharsulkar <salil.harsulkar@gmail.com>
2021-01-29 01:43:19 +0000

    updated to source input from a list

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -3,20 +3,41 @@ import logging
 from mylib import read_csv_to_list
 
 module_logger= logging.getLogger('my_application.make_dataset')
-def main():
+def main(data_files_list_path):
 
     module_logger.info('Starting execution of make_dataset module.')
-
-    required_features=read_csv_to_list('https://raw.githubusercontent.com/sharsulkar/H1B_LCA_outcome_prediction/main/data/processed/required_features.csv',header=None,squeeze=True)
-    try :
-        #input_df=pd.read_excel('https://www.dol.gov/sites/dolgov/files/ETA/oflc/pdfs/LCA_Disclosure_Data_FY2020_Q2.xlsx',usecols=required_features)
-        input_df=pd.read_excel('https://github.com/sharsulkar/H1B_LCA_outcome_prediction/raw/main/data/interim/LCA_dataset_sample1000.xlsx',usecols=required_features)
-    except ValueError:
-        module_logger.exception('ValueError: columns %s not found',required_features)
     
-    module_logger.info('Execution of make_dataset module complete.')
+    #import the required features list
+    required_features=read_csv_to_list('https://raw.githubusercontent.com/sharsulkar/H1B_LCA_outcome_prediction/main/data/processed/required_features.csv',header=None,squeeze=True)
 
+    #create an empty dataframe to hold the final concatenated result
+    input_df=pd.DataFrame(columns=required_features)
+
+    #define the file object 
+    file_itr=open(file=data_files_list_path,mode='r')
+
+    #iterate through the file and append the data to input_df
+    for path in file_itr:
+
+        try :
+            #input_df=pd.read_excel('https://www.dol.gov/sites/dolgov/files/ETA/oflc/pdfs/LCA_Disclosure_Data_FY2020_Q2.xlsx',usecols=required_features)
+            #input_df=pd.read_excel('https://github.com/sharsulkar/H1B_LCA_outcome_prediction/raw/main/data/interim/LCA_dataset_sample1000.xlsx',usecols=required_features)
+            data_df=pd.read_excel(path,usecols=required_features)
+
+            module_logger.info('Imported dataframe with shape [%d,%d]',data_df.shape[0],data_df.shape[1])
+
+            input_df=input_df.append(data_df,ignore_index=True)
+        except ValueError:
+            module_logger.exception('ValueError: columns %s not found',required_features)
+    
+    file_itr.close
+
+    module_logger.info('Final input dataframe is of shape [%d,%d]',input_df.shape[0],input_df.shape[1])
+    module_logger.info('Execution of make_dataset module complete.')
+    
     return input_df
 
 if __name__ == '__main__':
-    main()
+    data_files_list_path='./data/interim/LCA_files_list.txt'
+    input_df=main(data_files_list_path)
+    print(input_df.shape)
