fb8e48fea8ec3ef3143d446fd6c100e454edf462
sharsulkar <salil.harsulkar@gmail.com>
2021-02-05 03:01:06 +0000

    generalized code to make it reusable during training, prediction & eval

--- src/build_features.py
+++ src/build_features.py
@@ -11,80 +11,151 @@ from pickle import dump, load
 import transforms 
 from mylib import read_csv_to_list
 
-#import observations_df for referencing features and corresponding preprocessing actions to be performed on them
-#observations_df=pd.read_csv('https://raw.githubusercontent.com/sharsulkar/H1B_LCA_outcome_prediction/main/reports/final_observations.csv',sep='$',index_col=0,error_bad_lines=False)
+
 module_logger= logging.getLogger('my_application.build_features')
 
-def main(input_df):
+def main(input_df, build_feature_pipe=None, all_preprocess=None, method='fit_transform'):
+    
 
-    module_logger.info('Starting to build features.')
+    module_logger.info('Starting to build features module.')
 
     #identify and define column sets for applying preprocessing transforms
-    num_cols=read_csv_to_list('https://raw.githubusercontent.com/sharsulkar/H1B_LCA_outcome_prediction/main/data/processed/numeric_columns.csv',header=None,squeeze=True)
-    cat_cols=read_csv_to_list('https://raw.githubusercontent.com/sharsulkar/H1B_LCA_outcome_prediction/main/data/processed/categorical_columns.csv',header=None,squeeze=True)
-    drop_cols=read_csv_to_list('https://github.com/sharsulkar/H1B_LCA_outcome_prediction/raw/main/data/processed/drop_columns.csv',header=None,squeeze=True)
-    fe_cols=read_csv_to_list('https://raw.githubusercontent.com/sharsulkar/H1B_LCA_outcome_prediction/main/data/processed/feature_engineering_columns.csv',header=None,squeeze=True)
+    num_cols=read_csv_to_list('./data/processed/numeric_columns.csv',header=None,squeeze=True)
+    cat_cols=read_csv_to_list('./data/processed/categorical_columns.csv',header=None,squeeze=True)
+    drop_cols=read_csv_to_list('./data/processed/drop_columns.csv',header=None,squeeze=True)
+    fe_cols=read_csv_to_list('./data/processed/feature_engineering_columns.csv',header=None,squeeze=True)
         
     module_logger.info('Importing columns from stored lists complete.')
 
-    drop_row_index=input_df[~input_df.CASE_STATUS.isin(['Certified','Denied'])].index
-    module_logger.info('Number of rows with CASE_STATUS other than Certified and Denied:%d',drop_row_index.shape[0])
+    #Build preprocessing pipeline 
+    if method in ['fit','fit_transform'] and build_feature_pipe is None:
+        module_logger.info('Building build_features_pipe for very first time.')
+        build_feature_pipe=make_pipeline(
+            transforms.DropRowsTransformer(),
+            transforms.BuildFeaturesTransformer(fe_cols)
+            )
 
-    #Build preprocessing pipeline
-    build_feature_pipe=make_pipeline(
-        transforms.DropRowsTransformer(row_index=drop_row_index,inplace=True,reset_index=True),
-        transforms.BuildFeaturesTransformer(fe_cols)
+    if method in ['fit','fit_transform'] and all_preprocess is None:
+        module_logger.info('Building all_preprocess_pipe for very first time.')
+
+        numerical_preprocess=make_pipeline(
+            SimpleImputer(strategy='median'),
+            CustomStandardScaler()
         )
 
-    numerical_preprocess=make_pipeline(
-        SimpleImputer(strategy='mean'),
-        StandardScaler()
-    )
+        preprocess_pipe=make_column_transformer(
+            (transforms.DropFeaturesTransformer(columns=list(drop_cols),inplace=True),list(drop_cols)),
+            (transforms.RandomStandardEncoderTransformer(cat_cols),cat_cols),
+            (numerical_preprocess,num_cols),
+            remainder='passthrough'
+        )
 
-    preprocess_pipe=make_column_transformer(
-        (transforms.DropFeaturesTransformer(columns=list(drop_cols),inplace=True),list(drop_cols)),
-        (transforms.RandomStandardEncoderTransformer(cat_cols),cat_cols),
-        (numerical_preprocess,num_cols),
-        remainder='passthrough'
-    )
-
-    all_preprocess=make_pipeline(
-        preprocess_pipe
-    )
-
+        all_preprocess=make_pipeline(
+            preprocess_pipe
+        )
 
     #apply pipeline
-    module_logger.info('Pipeline started')
-    #feature engineering + drop rows
-    fe_df=build_feature_pipe.fit_transform(input_df)
-    #Ensure that there are no records with CASE_STATUS not in ['Certified','Denied'] after drop_rows  
-    assert fe_df[~fe_df.CASE_STATUS.isin(['Certified','Denied'])].shape[0]==0, module_logger.error('Unexpected values found in CASE_STATUS field.')
+    module_logger.info('Pipeline started') 
 
-    module_logger.info('feature engineering + drop rows done')
+    if method=='fit':
+        module_logger.info('Starting pipeline.fit')
 
-    #Separate target column - add conditions to apply only on training dataset
-    y=fe_df.pop('CASE_STATUS')
-    module_logger.info('Target column separated')
+        build_feature_pipe.fit(input_df) 
+        all_preprocess.fit(input_df)
+        
+        X=[] #return empty array as only pipeline is fitted
+        y=[] #return empty array as only pipeline is fitted
+        module_logger.info('Pipeline.fit completed successfully')
 
-    #drop columns + encoding
-    X=all_preprocess.fit_transform(fe_df)
+    elif method=='transform':
 
-    #Ensure that X has expected number of features  
-    assert X.shape[1]==31,module_logger.exception('Arrays X of shape [:,31] expected.')
-    #Ensure that X and y have same number of rows  
-    assert X.shape[0]==y.shape[0],module_logger.exception('Arrays X and y should have same number of rows.') 
-    module_logger.info('drop columns + encoding done')
+        module_logger.info('Starting pipeline.transform')
 
-    #save transformed dataset and target 
-    #pd.DataFrame(X,columns=fe_df.columns.values).to_csv('/content/drive/MyDrive/Datasets/processed.csv') 
+        #build_feature_pipe, all_preprocess cannot be None
+        assert build_feature_pipe!=None, module_logger.error('Missing pipeline object build_feature_pipe.')
+        assert all_preprocess!=None, module_logger.error('Missing pipeline object all_preprocess.')
+
+        fe_df=build_feature_pipe.transform(input_df) 
+        module_logger.info('feature engineering + drop rows done')
+
+        #Check if input_df has column CASE_STATUS, dataset during prediction will not have target variable so below code should be skipped
+        if 'CASE_STATUS' in fe_df.columns.values: 
+            module_logger.info('Target column found.')
+
+            assert fe_df[~fe_df.CASE_STATUS.isin(['Certified','Denied'])].shape[0]==0, module_logger.error('Unexpected values found in CASE_STATUS field.')
+            y=fe_df.pop('CASE_STATUS')
+            y.replace(['Certified','Denied'],[0,1],inplace=True)
+
+            module_logger.info('Target column separated')
+        #if CASE_STATUS is not present, return empty array for y
+        else:
+            module_logger.info('Target column not found. Returning empty array for y.')
+            y=[]
+
+        X=all_preprocess.transform(fe_df)
+
+        #Ensure that X has expected number of features  
+        assert X.shape[1]==31,module_logger.exception('Arrays X of shape [:,31] expected.')
+        #Ensure that X and y have same number of rows  
+        assert X.shape[0]==y.shape[0],module_logger.exception('Arrays X and y should have same number of rows.') 
+
+        module_logger.info('drop columns + encoding done')
+        module_logger.info('pipeline.transform completed successfully')
+
+    elif method=='fit_transform':
+        module_logger.info('Starting pipeline.fit_transform')
+        fe_df=build_feature_pipe.fit_transform(input_df) 
+        module_logger.info('feature engineering + drop rows done')
+
+        assert fe_df[~fe_df.CASE_STATUS.isin(['Certified','Denied'])].shape[0]==0, module_logger.error('Unexpected values found in CASE_STATUS field.')
+        y=fe_df.pop('CASE_STATUS')
+        y.replace(['Certified','Denied'],[0,1],inplace=True)
+        module_logger.info('Target column separated')
+
+        X=all_preprocess.fit_transform(fe_df)
+
+        #Ensure that X has expected number of features  
+        assert X.shape[1]==31,module_logger.exception('Arrays X of shape [:,31] expected.')
+        #Ensure that X and y have same number of rows  
+        assert X.shape[0]==y.shape[0],module_logger.exception('Arrays X and y should have same number of rows.') 
+
+        module_logger.info('drop columns + encoding done')
+        module_logger.info('pipeline.fit_transform completed successfully')
+
+    elif method=='inverse':
+        module_logger.info('Starting pipeline.inverse_transform')
+
+        # all_preprocess cannot be None
+        assert all_preprocess!=None, module_logger.error('Missing pipeline object all_preprocess.')
+
+        X=all_preprocess.inverse_transform(input_df)
+        y=[] #return empty array as inverse transform only applies to input features
+        module_logger.info('drop columns + encoding done')
+        module_logger.info('pipeline.inverse_transform completed successfully')
+
+    
     module_logger.info('Building features complete.')
-    #save pipeline
-    #dump(all_preprocess,open('/content/drive/MyDrive/preprocess_pipe.pkl','wb')) 
-    ##all_preprocess=load(open('/content/drive/MyDrive/preprocess_pipe.pkl','rb')) 
+    
+    #save pipeline when method is fit, fit_transform
+    if method in ['fit','fit_transform']:
+        dump(build_feature_pipe,open('./models/build_feature_pipe.pkl','wb')) 
+        dump(all_preprocess,open('./models/preprocess_pipe.pkl','wb')) 
+        module_logger.info('Pipeline saved.')
+    
     return X, y
 
 if __name__ == '__main__':
     import make_dataset
+    from pickle import load
 
-    input_df=make_dataset.main()
+    data_files_list_path='./data/interim/LCA_files_list.txt'
+
+    input_df=make_dataset.main(data_files_list_path)
+
+    #For training the very first time
     main(input_df)
+
+    #For iterative training
+    #build_feature_pipe=load(open('./models/build_feature_pipe.pkl','rb')) 
+    #all_preprocess=load(open('./models/preprocess_pipe.pkl','rb')) 
+    #main(input_df,build_feature_pipe,all_preprocess)
--- src/make_dataset.py
+++ src/make_dataset.py
@@ -3,32 +3,37 @@ import logging
 from mylib import read_csv_to_list
 
 module_logger= logging.getLogger('my_application.make_dataset')
-def main(data_files_list_path):
+def main(path,file_type='file_list'):
 
     module_logger.info('Starting execution of make_dataset module.')
     
     #import the required features list
-    required_features=read_csv_to_list('https://raw.githubusercontent.com/sharsulkar/H1B_LCA_outcome_prediction/main/data/processed/required_features.csv',header=None,squeeze=True)
+    required_features=read_csv_to_list('./data/processed/required_features.csv',header=None,squeeze=True)
 
-    #create an empty dataframe to hold the final concatenated result
-    input_df=pd.DataFrame(columns=required_features)
+    if file_type=='file_list':
+        #create an empty dataframe to hold the final concatenated result
+        input_df=pd.DataFrame(columns=required_features)
 
-    #define the file object 
-    file_itr=open(file=data_files_list_path,mode='r')
+        #define the file object 
+        file_itr=open(file=path,mode='r')
 
-    #iterate through the file and append the data to input_df
-    for path in file_itr:
+        #iterate through the file and append the data to input_df
+        for file_path in file_itr:
 
-        try :
-            data_df=pd.read_excel(path,usecols=required_features)
+            try :
+                data_df=pd.read_excel(file_path,usecols=required_features)
 
-            module_logger.info('Imported dataframe with shape [%d,%d]',data_df.shape[0],data_df.shape[1])
+                module_logger.info('Imported dataframe with shape [%d,%d]',data_df.shape[0],data_df.shape[1])
 
-            input_df=input_df.append(data_df,ignore_index=True)
-        except ValueError:
-            module_logger.exception('ValueError: columns %s not found',required_features)
+                input_df=input_df.append(data_df,ignore_index=True)
+            except ValueError:
+                module_logger.exception('ValueError: columns %s not found',required_features)
+        
+        file_itr.close
     
-    file_itr.close
+    elif file_type=='data_file':
+        input_df=pd.read_excel(path,usecols=required_features)
+
 
     module_logger.info('Final input dataframe is of shape [%d,%d]',input_df.shape[0],input_df.shape[1])
     module_logger.info('Execution of make_dataset module complete.')
@@ -37,5 +42,5 @@ def main(data_files_list_path):
 
 if __name__ == '__main__':
     data_files_list_path='./data/interim/LCA_files_list.txt'
-    input_df=main(data_files_list_path)
+    input_df=main(path=data_files_list_path,file_type='file_list')
     print(input_df.shape)
