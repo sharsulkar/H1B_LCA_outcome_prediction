fb3409fc3a3196a859b9036f554fd02c06abf21b
sharsulkar <salil.harsulkar@gmail.com>
2021-03-02 20:21:46 +0000

    apt-install

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -15,7 +15,8 @@
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-pip
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme 
+apt install python3-pip
 
 pip install requirements.txt
  
